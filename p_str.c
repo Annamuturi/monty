7afb15e6fda5ecaf60b44f6bbe22143cb0e828e4
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 12:53:23 -0700

    pstr

--- p_str.c
+++ p_str.c
@@ -3,7 +3,7 @@
 /**
  * pstr - prints the string starting at the top of the stack
  *
- * @stack: poiunter to list
+ * @stack: pointer to list
  * @line_number: number of line where command was found
 */
 
@@ -12,12 +12,16 @@ void pstr(stack_t **stack, unsigned int line_number)
 	stack_t *temp = NULL;
 	(void)line_number;
 
+	/*  Assign the stack pointer to a temporary variable */
 	temp = *stack;
+
+	/* If the stack is empty, print a newline character and return */
 	if (!temp)
 	{
 		putchar('\n');
 		return;
 	}
+	/* Iterate through the stack and print the characters*/
 	while (temp)
 	{
 		if (temp->n > 31 && temp->n < 127)
@@ -26,5 +30,6 @@ void pstr(stack_t **stack, unsigned int line_number)
 			break;
 		temp = temp->next;
 	}
+	/* Print a newline character after printing the string */
 	putchar('\n');
 }
