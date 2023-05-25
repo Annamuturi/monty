e46aa3c770df00605136fdce92e875c9378cd831
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 11:06:08 -0700

    multiply

--- multiply.c
+++ multiply.c
@@ -1,24 +1,30 @@
 #include "alx_monty.h"
+
 /**
- * mul - multiplies top two elements, stores sum in
- * second to top and pops top element
+ * mul - multiply top two elements and pops top element
  *
  * @stack: poiunter to list
  * @line_number: number of line where command was found
  */
+
 void mul(stack_t **stack, unsigned int line_number)
 {
+	/* variable declaration */
 	stack_t *temp = *stack;
 	stack_t *counter = *stack;
 	int i = 0;
 
+	/* Count the number of elements in the stack */
 	for (; counter; i++)
 		counter = counter->next;
+	/* Check if the stack has at least 2 elements or if it's empty */
 	if (i < 2 || !*stack || !(*stack)->next || !stack)
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
-		frees_struct(stack);
+		frees_struct(stack); /* free memory */
 	}
+	/* Multiply the values elements and store the in the second element */
 	temp->next->n = temp->n * temp->next->n;
+	/* Pop the top element from the stack*/
 	pop(stack, line_number);
 }
