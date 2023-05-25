8a5e96128ccf2a27b276f84b1b76020fbda4eb0c
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 09:05:53 -0700

    pchar

---
+++ pchar.c
@@ -0,0 +1,26 @@
+# include "monty.h"
+/**
+ * pchar - prints the char at the top of the stack
+ *
+ * @stack: poiunter to list
+ * @line_number: number of line where command was found
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		frees_struct(stack);
+	}
+	if ((*stack)->n > 31 && (*stack)->n < 127)
+	{
+		putchar((*stack)->n);
+		putchar('\n');
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		frees_struct(stack);
+	}
+}
