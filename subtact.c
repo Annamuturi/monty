ba90e837649d3683edad0e83f3b7115d8c6e6026
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 13:54:32 -0700

    subtract

--- subtact.c
+++ subtact.c
@@ -11,14 +11,14 @@ void sub(stack_t **stack, unsigned int line_number)
 {
 	/* variable declaration */
 	stack_t *tmp = *stack;
-	stack_t *counter = *stack;
-	int i = 0;
+	stack_t *count = *stack;
+	int a = 0;
 
 	/* Count the number of elements in the stack */
-	for (; counter; i++)
-		counter = counter->next;
+	for (; count; i++)
+		count = count->next;
 	/* Check if the stack has at least 2 elements or if it's empty */
-	if (i < 2 || !*stack || !(*stack)->next || !stack)
+	if (a < 2 || !*stack || !(*stack)->next || !stack)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		frees_struct(stack); /* free memory */
