cda9c5acb6eacf1a112fa33af6f7efe7f91602b3
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 14:21:34 -0700

    function

--- func.c
+++ func.c
@@ -3,12 +3,12 @@
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list
  *
- * @head: double pointer to double linked list
+ * @anna: double pointer to double linked list
  * @n: data to store
  * Return: the address of the new element, or NULL if it failed
  */
 
-stack_t *add_dnodeint(stack_t **head, const int n)
+stack_t *add_dnodeint(stack_t **anna, const int n)
 {
 	stack_t *new_node = NULL;
 	/* Allocate memory for the new node */
@@ -19,41 +19,41 @@ stack_t *add_dnodeint(stack_t **head, const int n)
 		exit(EXIT_FAILURE);
 	}
 	/* Set the values for the new node */
-	new_node->next = *head;
+	new_node->next = *anna;
 	new_node->n = n;
 	new_node->prev = NULL;
-	/* Update the head of the list */
-	if (*head == NULL)
+	/* Update the anna of the list */
+	if (*anna == NULL)
 	{
-		*head = new_node;
+		*anna = new_node;
 	}
 	else
 	{
 		/*If the list is not empty, update the prev pointer*/
-		/*  and update the head pointer to the new node */
-		(*head)->prev = new_node;
-		*head = new_node;
+		/*  and update the anna pointer to the new node */
+		(*anna)->prev = new_node;
+		*anna = new_node;
 	}
 	return (new_node);
 }
 
 /**
  * free_stack_t - frees list
- * @head: pointer to head of list
+ * @anna: pointer to head of list
  */
 
-void free_stack_t(stack_t **head)
+void free_stack_t(stack_t **anna)
 {
 	stack_t *traverse;
 	/* Check if the list is empty */
 
-	if (*head == NULL)
+	if (*anna == NULL)
 		return;
 	/* Free the memory for each node in the list */
-	while (*head != NULL)
+	while (*anna != NULL)
 	{
-		traverse = *head;
-		(*head) = (*head)->next;
+		traverse = *anna;
+		(*anna) = (*anna)->next;
 		free(traverse);
 	}
 }
