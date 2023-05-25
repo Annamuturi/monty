c72b1b4b251e82325397722cbc5e369f7306b8e1
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 12:36:46 -0700

    pchar

--- pchar.c
+++ pchar.c
@@ -1,18 +1,22 @@
-# include "alx_monty.h"
+#include "alx_monty.h"
+
 /**
  * pchar - prints the char at the top of the stack
  *
- * @stack: poiunter to list
+ * @stack: pointer to list
  * @line_number: number of line where command was found
  */
+
 void pchar(stack_t **stack, unsigned int line_number)
 {
+	/* Check if the stack is empty */
 
 	if (!stack || !*stack)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		frees_struct(stack);
 	}
+	/*Check if the value at the top of the stack is within the ASCII range*/
 	if ((*stack)->n > 31 && (*stack)->n < 127)
 	{
 		putchar((*stack)->n);
