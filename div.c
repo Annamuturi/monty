4d01362e1f73f80a96f26a0f8005519a4d44e698
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 13:35:58 -0700

    subtraction

--- div.c
+++ div.c
@@ -11,14 +11,14 @@ void division(stack_t **stack, unsigned int line_number)
 {
 	/* variable declaration */
 	stack_t *tmp = *stack;
-	stack_t *counter = *stack;
-	int i = 0;
+	stack_t *count = *stack;
+	int a = 0;
 
 	/* Count the number of elements in the stack */
-	for (; counter; i++)
-		counter = counter->next;
+	for (; count; ++)
+		count = count->next;
 	/* Check if the stack has at least 2 elements or if it's empty */
-	if (i < 2 || !*stack || !(*stack)->next || !stack)
+	if (a < 2 || !*stack || !(*stack)->next || !stack)
 	{
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		frees_struct(stack);
