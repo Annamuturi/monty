01305f4deb8803f08aaf8bddb7b1c6eed9566236
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 13:49:35 -0700

    multiply

--- multiply.c
+++ multiply.c
@@ -11,14 +11,14 @@ void mul(stack_t **stack, unsigned int line_number)
 {
 	/* variable declaration */
 	stack_t *tmp = *stack;
-	stack_t *counter = *stack;
+	stack_t *count = *stack;
 	int i = 0;
 
 	/* Count the number of elements in the stack */
-	for (; counter; i++)
-		counter = counter->next;
+	for (; count; a++)
+		count = count->next;
 	/* Check if the stack has at least 2 elements or if it's empty */
-	if (i < 2 || !*stack || !(*stack)->next || !stack)
+	if (a < 2 || !*stack || !(*stack)->next || !stack)
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		frees_struct(stack); /* free memory */
