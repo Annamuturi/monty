f3ef5213dca190655861acefc929e1c3de089882
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 12:40:04 -0700

    pall

--- p_pall.c
+++ p_pall.c
@@ -3,17 +3,20 @@
 /**
  * pall - prints list
  *
- * @stack: poiunter to list
+ * @stack: pointer to list
  * @line_number: number of line where command was found
  */
+
 void pall(stack_t **stack, unsigned int line_number)
 {
 	const stack_t *temp = *stack;
 	(void)line_number;
 
+	/*Check if the stack is empty*/
 	if (!stack)
 		return;
 
+	/* Traverse the stack and print each element */
 	while (temp)
 	{
 		printf("%d\n", temp->n);
