a192bed65a7640a0727d6908a45e4a422c9e6375
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 09:22:27 -0700

    nop

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "alx_monty.c"
+
+/**
+ * nop - does nothing
+ *
+ * @stack: poiunter to list
+ * @line_number: number of line where command was found
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
