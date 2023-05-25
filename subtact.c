e5b1914a82625a1cba242db455111cbf805da823
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 11:19:17 -0700

    subtract

--- subtact.c
+++ subtact.c
@@ -1,25 +1,30 @@
 #include "alx_monty.h"
 
 /**
- * sub - subs top two elements, stores sum in second
- * to top and pops top element
+ * sub - subs top two elements and pops top element
  *
  * @stack: poiunter to list
  * @line_number: number of line where command was found
  */
+
 void sub(stack_t **stack, unsigned int line_number)
 {
+	/* variable declaration */
 	stack_t *temp = *stack;
 	stack_t *counter = *stack;
 	int i = 0;
 
+	/* Count the number of elements in the stack */
 	for (; counter; i++)
 		counter = counter->next;
+	/* Check if the stack has at least 2 elements or if it's empty */
 	if (i < 2 || !*stack || !(*stack)->next || !stack)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
-		frees_struct(stack);
+		frees_struct(stack); /* free memory */
 	}
+	/* Subtract the value element  and store the result in the second element */
 	temp->next->n = temp->next->n - temp->n;
+	/*  Pop the top element from the stack */
 	pop(stack, line_number);
 }
