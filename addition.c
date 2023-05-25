1511bbc86b1c238c1ef2e2d82e75ca9cfdd5bb68
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 13:31:32 -0700

    addition

--- addition.c
+++ addition.c
@@ -13,13 +13,13 @@ void add(stack_t **stack, unsigned int line_number)
 	/* variable declaration */
 	/* Temporary variable to hold the top element */
 	stack_t *tmp = *stack;
-	/* Counter variable to count the number of elements */
-	stack_t *counter = *stack;
+	/* Count variable to count the number of elements */
+	stack_t *count = *stack;
 	int i = 0;
 
 	/* Count the number of elements in the stack */
-	for (; counter; i++)
-		counter = counter->next;
+	for (; count; i++)
+		count = count->next;
 	/* Check if the stack has at least 2 elements or if it's empty */
 	if (i < 2 || !*stack || !(*stack)->next || !stack)
 	{
