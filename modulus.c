4407c7c78ba9465146159b57d69b73f3e064c4c3
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 10:51:03 -0700

    modulus

--- modulus.c
+++ modulus.c
@@ -1,30 +1,36 @@
 #include "alx_monty.h"
 
 /**
- * mod - returns whats left of division of top two elements,
- * stores sum in second to top and pops top element
+ * mod - returns whats left of divi elements and pops top element
  *
  * @stack: poiunter to list
  * @line_number: number of line where command was found
  */
+
 void mod(stack_t **stack, unsigned int line_number)
 {
+	/* variable declaration */
 	stack_t *temp = *stack;
 	stack_t *counter = *stack;
 	int i = 0;
 
+	/* Count the number of elements in the stack */
 	for (; counter; i++)
 		counter = counter->next;
+	/* Check if the stack has at least 2 elements or if it's empty */
 	if (i < 2 || !*stack || !(*stack)->next || !stack)
 	{
 		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		frees_struct(stack);
 	}
+	/* Check if the top element of the stack is zero */
 	if (temp->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
 		frees_struct(stack);
 	}
+	/* Calculate the modulo of the element and store  in the second element */
 	temp->next->n = temp->next->n % temp->n;
+	/* Pop the top element from the stack */
 	pop(stack, line_number);
 }
