35ced3b9c2c346e41f50dfa0db6460c52374aa21
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 12:27:23 -0700

    function_one

--- func_one.c
+++ func_one.c
@@ -1,22 +1,27 @@
 #include "alx_monty.h"
+
 /**
  * file_open - opens bytecode file
  * @argc: argument counter
  * @argv: arguements
+ *
  * Return: file pointer
  */
+
 void file_open(int argc, char **argv)
 {
 	FILE *check = NULL;
 
 	if (argc == 2)
 	{
+		/* Open the file in read mode */
 		check = fopen(argv[1], "r");
 		if (check == NULL)
 		{
 			fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 			exit(EXIT_FAILURE);
 		}
+		/* Store the file pointer in command_t for later use */
 		command_t.store_check = check;
 	}
 	else
@@ -26,34 +31,41 @@ void file_open(int argc, char **argv)
 	}
 }
 
+
 /**
  * isdigit_str - checks if a string contains all numbers
  * @token: string
  * Return: 0 if no number, 1 if numbers
  */
+
 int isdigit_str(char *token)
 {
 	int i = 0;
+	/* Check if the first character is a negative sign */
 
 	if (token == NULL)
 		return (0);
 
 	if (token[i] == '-')
 		i++;
-
+	/* Iterate through the characters of the string */
 	while (token[i])
 	{
+		/* Check if the character is not a digit */
 		if (isdigit(token[i]) == 0)
 			return (0);
 		i++;
 	}
+	/* All characters are digits */
 	return (1);
 }
+
 /**
  * frees_struct - frees elements
  *
  * @stack: pointer to list
  */
+
 void frees_struct(stack_t **stack)
 {
 	free(command_t.instructions);
@@ -62,6 +74,7 @@ void frees_struct(stack_t **stack)
 	fclose(command_t.store_check);
 	exit(EXIT_FAILURE);
 }
+
 /**
  * free_all - frees most elements
  *
@@ -70,6 +83,7 @@ void frees_struct(stack_t **stack)
  * @commands: pointer to commands
  * @check: file pointer
  */
+
 void free_all(char *line, stack_t *head, char **commands, FILE *check)
 {
 	free(line);
