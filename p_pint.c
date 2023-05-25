a32119c2ddf5803f7d414368ff1e85554a99843f
Annamuturi <muturiwannah83@gmail.com>
2023-05-25 12:47:45 -0700

    pint

--- p_pint.c
+++ p_pint.c
@@ -3,11 +3,13 @@
 /**
  * pint - prints top element
  *
- * @stack: poiunter to list
+ * @stack: pointer to list
  * @line_number: number of line where command was found
  */
+
 void pint(stack_t **stack, unsigned int line_number)
 {
+	/* Check if the stack is empty or if the stack pointer is NULL */
 	if (!*stack || !stack)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
@@ -16,5 +18,6 @@ void pint(stack_t **stack, unsigned int line_number)
 		fclose(command_t.store_check);
 		exit(EXIT_FAILURE);
 	}
+	/* Print the value of the top element of the stack */
 	printf("%d\n", (*stack)->n);
 }
